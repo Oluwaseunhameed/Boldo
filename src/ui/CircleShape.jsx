7614531051a45f0bfa3a6ad47ab5613143027526
Oluwaseun Hameed <hameedoluwaseun@gmail.com>
2024-10-20 07:27:46 +0100

    Update halfCircle

--- src/ui/CircleShape.jsx
+++ src/ui/CircleShape.jsx
@@ -4,11 +4,11 @@ const HalfCircle = styled.div`
   position: absolute;
   top: ${({ top }) => top || 0};
   right: ${({ right }) => right || 0};
-  width: ${({ width }) => width || "70rem"};
-  height: ${({ height }) => height || "60rem"};
+  width: ${({ width }) => width || "110rem"};
+  height: ${({ height }) => height || "110rem"};
   background-color: ${({ bgColor }) => bgColor || "var(--color-grey-25)"};
   z-index: 1;
-  clip-path: ${({ halfCircle }) => halfCircle || "circle(25% at 100% 0)"};
+  clip-path: ${({ halfCircle }) => halfCircle || "circle(50% at 100% 0)"};
 `;
 
 export default HalfCircle;
